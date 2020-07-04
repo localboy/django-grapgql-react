53cf082dbfb52baf98034dfc794d4b99bfdcc16f
Mohammad Jakir Hossain <mjakir.ddd@gmail.com>
2020-07-04 18:47:00 +0600

    Added mutation to create user

--- myapp/schema.py
+++ myapp/schema.py
@@ -16,4 +16,32 @@ class Query(graphene.ObjectType):
 	    return UserModel.objects.all()
 
 
-schema = graphene.Schema(query=Query)
+class CreateUser(graphene.Mutation):
+    id = graphene.Int()
+    name = graphene.String()
+    last_name = graphene.String()
+
+    class Arguments:
+        name = graphene.String()
+        last_name = graphene.String()
+
+    def mutate(self, info, name, last_name):
+        user = UserModel(name=name, last_name=last_name)
+        user.save()
+
+        return CreateUser(
+            id=user.id,
+            name=user.name,
+            last_name=user.last_name
+        )
+
+
+class Mutation(graphene.ObjectType):
+    create_user = CreateUser.Field()
+
+
+
+schema = graphene.Schema(
+    query=Query,
+    mutation=Mutation
+)
